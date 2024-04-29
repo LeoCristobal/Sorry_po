6892d4bdf69f7f7af41ceb1c7f5c4a43831f9c72
LeoCristobal <164727261+LeoCristobal@users.noreply.github.com>
2024-04-29 18:33:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const alertBtn = document.querySelector('.alert-btn');
+
+alertBtn.addEventListener('click', () => {
+  alert("Sorry dun sa panaginip mo");
+  alert("kahit di ko nmn alam na ganun ako sa panaginip mo");
+  alert("pero mag sosorry nlng ako");
+  alert("don't worry my love");
+  alert("your man will never do that,");
+  alert("your man will never leave you,");
+  alert("your man will never cheat on you.");
+  alert("Alam mo nmn kung gaano ako ka-loyal sayo");
+  alert("kahit ni isang beses nga di ako nag isip na mag cheat/ iwan ka");
+  alert("kaya don't think na iiwan kita");
+  alert("ikaw lang nmn po palagi eh");
+  alert("Iloveyou so much bby ko mwaa");
+});
